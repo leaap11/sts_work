14b8752f9ad6dabc5187d7b0e23c83a32ed79046
leaap11 <programmer11@leaap.com>
2022-11-01 17:31:43 +0530

    1st commit

---
+++ src/main/java/com/example/demo/StsWorkApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsWorkApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Suresh Nallachi");
+		SpringApplication.run(StsWorkApplication.class, args);
+	}
+
+}
